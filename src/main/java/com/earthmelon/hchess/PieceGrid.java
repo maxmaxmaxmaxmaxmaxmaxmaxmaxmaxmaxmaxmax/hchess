c5b46c7efabf8e0118b54179ccc6f6d6565468c4
earth-melon <sparrowkingwar1@gmail.com>
2026-08-31 15:34:19 +1000

    added selecting piece based on mouse coordinates

--- src/main/java/com/earthmelon/hchess/PieceGrid.java
+++ src/main/java/com/earthmelon/hchess/PieceGrid.java
@@ -8,6 +8,12 @@ import static com.earthmelon.hchess.Piece.*;
 
 public class PieceGrid {
 
+    // Size of one square in pixels
+    public final int gridSize = 80;
+
+    public final int topLeftWidth = 320;
+    public final int topLeftHeight = 60;
+
     Mesh background = MeshLoader.createQuad(new Vector3f(-0.5f,-0.75f,0)).addTexture("board.png");;
     static Piece[] boardState = {wROOK, wKNIGHT, wBISHOP, wQUEEN, wKING, wBISHOP, wKNIGHT, wROOK,
                 wPAWN, wPAWN, wPAWN, wPAWN, wPAWN, wPAWN, wPAWN, wPAWN,
@@ -18,15 +24,25 @@ public class PieceGrid {
                 bPAWN, bPAWN, bPAWN, bPAWN, bPAWN, bPAWN, bPAWN, bPAWN,
         bROOK, bKNIGHT, bBISHOP, bQUEEN, bKING, bBISHOP, bKNIGHT, bROOK};
 
-    public Piece selectPiece(int row, int column) {
+    public Piece getPiece(int row, int column) {
         return boardState[8*row+column];
+    }
+
+    public Piece selectPiece(int mouseX, int mouseY) {
+        if (mouseX < topLeftWidth || mouseX > topLeftWidth + 8 * gridSize) {
+            return null;
+        }
+        if (mouseY < topLeftHeight || mouseY > topLeftHeight + 8 * gridSize) {
+            return null;
+        }
+        return getPiece(mouseX / gridSize, mouseY / gridSize);
     }
 
     public void renderBoard() {
         for (int i=0; i<boardState.length; i++) {
             int row = i / 8;
             int col = i % 8;
-            Piece piece = selectPiece(row, col);
+            Piece piece = getPiece(row, col);
             if (piece != null) {
                 Vector3f square = new Vector3f((float) col / 8 - 0.5f, (float) row / 5 - 0.75f,0);
                 MeshLoader.createQuad(square, 1f/8).addTexture(piece.getTexture());
