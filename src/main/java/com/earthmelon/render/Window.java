fe5736b30e1852d947ebcb088d967423007b934c
earthmelon <sparrowkingwar1@gmail.com>
2026-09-07 15:07:52 +1000

    added alpha

--- src/main/java/com/earthmelon/render/Window.java
+++ src/main/java/com/earthmelon/render/Window.java
@@ -89,6 +89,11 @@ public class Window {
             GLFW.glfwShowWindow(window);
         }
 
+        GL11.glEnable(GL11.GL_BLEND); // Enabled blending
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA); // selects blending method
+        GL11.glEnable(GL11.GL_ALPHA_TEST); // allows alpha channels or transperancy
+        GL11.glAlphaFunc(GL11.GL_GREATER, 0.1f); // sets aplha function
+
         GL.createCapabilities();
     }
 
