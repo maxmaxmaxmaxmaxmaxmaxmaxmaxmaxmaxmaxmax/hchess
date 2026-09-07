585e305edef75ce8e5947111ba2371519aaf206e
earthmelon <sparrowkingwar1@gmail.com>
2026-09-07 15:14:52 +1000

    fixed some rendering issues

--- src/main/java/com/earthmelon/hchess/PieceGrid.java
+++
@@ -1,9 +0,0 @@
-package com.earthmelon.hchess;
-
-import com.earthmelon.render.Mesh;
-
-public class PieceGrid {
-
-    Mesh background;
-    Piece[][] boardState = new Piece[8][8];
-}
--- src/main/java/com/earthmelon/render/Window.java
+++ src/main/java/com/earthmelon/render/Window.java
@@ -89,12 +89,12 @@ public class Window {
             GLFW.glfwShowWindow(window);
         }
 
+        GL.createCapabilities();
+
         GL11.glEnable(GL11.GL_BLEND); // Enabled blending
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA); // selects blending method
         GL11.glEnable(GL11.GL_ALPHA_TEST); // allows alpha channels or transperancy
         GL11.glAlphaFunc(GL11.GL_GREATER, 0.1f); // sets aplha function
-
-        GL.createCapabilities();
     }
 
     public void update() {
